f813442f8313ea84e4e08d4206a536beabbce30f
kake01 <kakerun1123@gmail.com>
2019-10-16 17:27:13 +0900

    顔のトゲの改良前

--- js/chapter_img_random.js
+++ js/chapter_img_random.js
@@ -1,6 +1,7 @@
 var canvas = document.getElementById('rectangle');
 var ctx = canvas.getContext('2d');
-var head_num = 9;
+var head_num = 8;
+var head_big_radi = 70;
 var eye_control_y = 60;
 var eye_pos_y = 60;
 var eye_angle = 0;
@@ -9,11 +10,30 @@ var mouth_pos_y = 100;
 
 onload = function()
 {
-	CreateHead(ctx, head_num, 100, 80, 70);
+	CreateHead(ctx, head_num, 100, 80,head_big_radi);
 	CreateMouth(ctx, 60, mouth_pos_y, 140, mouth_pos_y, 100, mouth_control_y);
 	CreateEye(ctx, 70, eye_pos_y, 90, eye_pos_y, 80, eye_control_y, 40);
 };
 
+function CreateHead(context, n, dx, dy, radi_big)
+{
+	var angle_big = Math.PI / n;
+	var angle_small = 1/6 + Math.PI / n;
+	var radi_small = 40;
+
+	context.beginPath();
+	for(var i = 0; i < 2 * Math.PI; i += angle_big)
+	{
+		var big_x = Math.sin(i) * radi_big;
+		var big_y = Math.cos(i) * radi_big;
+		var small_x = Math.sin(1/6 + i) * radi_small;
+		var small_y = Math.cos(1/6 + i) * radi_small;
+		context.lineTo(big_x + dx,big_y + dy);
+		context.lineTo(small_x + dx,small_y + dy);
+	}
+	context.fillStyle = "#f00";
+	context.fill();
+}
 function CreateEye(context, start_x, start_y, end_x, end_y, control_x, control_y, eye_width)
 {
 	// 目玉あり
@@ -59,21 +79,6 @@ function CreateEye(context, start_x, start_y, end_x, end_y, control_x, control_y
 		context.fill();
 	}
 }
-function CreateHead(context, n, dx, dy, size)
-{
-	/*nは奇数、N角星 :dx,dyは座標: sizeは大きさ*/
-	context.beginPath();
-	var dig = Math.PI / n * 4;
-	for(var i = 0; i < n ; i++)
-	{
-		var x = Math.sin(i * dig);
-		var y = Math.cos(i * dig);
-		context.lineTo(x * size + dx, y * size + dy);
-	}
-	context.closePath();
-	context.fillStyle = "#f00";
-	context.fill();
-}
 function CreateMouth(context, start_x, start_y, end_x, end_y, control_x, control_y)
 {
 	//control_yで口の開き具合
@@ -84,6 +89,26 @@ function CreateMouth(context, start_x, start_y, end_x, end_y, control_x, control
 	context.lineWidth = 5;
 	context.stroke();
 }
+function good_head()
+{
+	// head_num -= 1;
+	head_big_radi -= 5;
+	CleateCharacter();
+}
+function bad_head()
+{
+	head_num += 1;
+	head_big_radi += 5;
+	CleateCharacter();
+	// if( 9 <= head_num)
+	// {
+	// 	head_num -= 2;
+	// 	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	// }
+}
+
+
 function good_eye()
 {
 	if(72 <= eye_control_y)
@@ -104,38 +129,23 @@ function bad_eye()
 	eye_pos_y -= 3;
 	CleateCharacter();
 }
-
-function bad_head()
-{
-	if( 9 <= head_num)
-	{
-		head_num -= 2;
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
-		CleateCharacter();
-	}
-}
-function good_head()
-{
-	head_num += 2;
-	CleateCharacter();
-}
-
-function bad_mouth()
-{
-	mouth_control_y -= 5;
-	mouth_pos_y +=3;
-	CleateCharacter();
-}
 function good_mouth()
 {
 	mouth_control_y += 5;
 	mouth_pos_y -=3;
 	CleateCharacter();
 }
+function bad_mouth()
+{
+	mouth_control_y -= 5;
+	mouth_pos_y +=3;
+	CleateCharacter();
+}
+
 function CleateCharacter()
 {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	CreateHead(ctx, head_num, 100, 80, 70);
+	CreateHead(ctx, head_num, 100, 80, head_big_radi);
 	CreateMouth(ctx, 60, mouth_pos_y, 140, mouth_pos_y, 100, mouth_control_y);
 	CreateEye(ctx, 70, eye_pos_y, 90, eye_pos_y, 80, eye_control_y, 40);
 }
