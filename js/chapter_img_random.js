41c842e7d10cdfb6c0bfbd4f5589126216a75721
kake01 <kakerun1123@gmail.com>
2019-10-31 17:24:19 +0900

    菌の複製

--- js/chapter_img_random.js
+++ js/chapter_img_random.js
@@ -1,47 +1,51 @@
 var canvas = document.getElementById('rectangle');
 var ctx = canvas.getContext('2d');
+
+var eye_width = 25;//目の長さ
+var eye_end_to_center = 25;//左目のenbとcenterとの距離
+var eye_to_eye = 35;// 目と目の距離
+
 var head_num = 8;
-var head_big_radi = 170;
+
+var eye_angle = 0;
+
+//キャラ1
 var chara_center_x = 375;
 var chara_center_y = 375;
-var eye_angle = 0;
-var eye_width = 50;
-
-//左目のenbとcenterとの距離
-var eye_end_to_center = 50;
-// 目と目の距離
-var eye_to_eye = 70;
 var eye_pos_y = chara_center_y - 50;
 var eye_control_y = chara_center_y - 50;
-var mouth_pos_y = chara_center_y + 50;
-var mouth_control_y = chara_center_y + 50;
+var mouth_pos_y = chara_center_y + 25;
+var mouth_control_y = chara_center_y + 25;
+var temp_temp = 0;
+//キャラごに違う
+var head_big_radi = 100;
+var radi_small = head_big_radi - 20;//固定
+var radi_inside = radi_small - 10;//固定
+
+
+//キャラ2
+var chara_center_x1 = 600;
+var chara_center_y1 = 650;
+var eye_pos_y1 = chara_center_y1 - 50;
+var eye_control_y1 = chara_center_y1 - 50;
+var mouth_pos_y1 = chara_center_y1 + 50;
+var mouth_control_y1 = chara_center_y1 + 50;
 
 onload = function()
 {
 	CreateHead(head_num, chara_center_x, chara_center_y, head_big_radi);
-	CreateMouth(chara_center_x - 100, mouth_pos_y, chara_center_x + 100, mouth_pos_y, mouth_control_y);
+	CreateMouth(chara_center_x - 50, mouth_pos_y, chara_center_x + 50, mouth_pos_y, mouth_control_y);
 	CreateEye(chara_center_x - (eye_end_to_center + eye_width),	eye_pos_y, chara_center_x - eye_end_to_center, eye_pos_y, eye_control_y, eye_to_eye + eye_end_to_center);
 
+	CreateHead(head_num, chara_center_x1, chara_center_y1, head_big_radi);
+	CreateMouth(chara_center_x1 - 50, mouth_pos_y1, chara_center_x1 + 50, mouth_pos_y1, mouth_control_y1);
+	CreateEye(chara_center_x1 - (eye_end_to_center + eye_width),	eye_pos_y1, chara_center_x1 - eye_end_to_center, eye_pos_y1, eye_control_y1, eye_to_eye + eye_end_to_center);
 };
 
 function CreateEye(start_x, start_y, end_x, end_y, control_y, eye_width)
 {
-	// 目玉あり
-	if(chara_center_y - 50/*初期値*/ < eye_control_y && eye_control_y <= 355/*悪いを10回押した*/)
-	{
-		ctx.beginPath();
-		//左目
-		ctx.moveTo(start_x, start_y);
-		ctx.quadraticCurveTo((start_x + end_x) / 2, control_y, end_x, end_y);
-		//右目
-		ctx.moveTo(start_x + eye_width, start_y);
-		ctx.quadraticCurveTo((start_x + end_x) / 2 + eye_width, control_y, end_x + eye_width, end_y);
-		ctx.fillStyle = '#CCCCCC';
-		ctx.fill();
-		ctx.closePath();
-	}
 	// ただの線
-	if(eye_control_y <= chara_center_y - 50/*初期値*/)
+	if(0 <= temp_temp/*初期値*/)
 	{
 		ctx.beginPath();
 		//左目
@@ -54,8 +58,22 @@ function CreateEye(start_x, start_y, end_x, end_y, control_y, eye_width)
 		ctx.lineWidth = 2;
 		ctx.stroke();
 	}
+	// 目玉あり
+	if(-10 < temp_temp && temp_temp < 0)
+	{
+		ctx.beginPath();
+		//左目
+		ctx.moveTo(start_x, start_y);
+		ctx.quadraticCurveTo((start_x + end_x) / 2, control_y, end_x, end_y);
+		//右目
+		ctx.moveTo(start_x + eye_width, start_y);
+		ctx.quadraticCurveTo((start_x + end_x) / 2 + eye_width, control_y, end_x + eye_width, end_y);
+		ctx.fillStyle = '#CCCCCC';
+		ctx.fill();
+		ctx.closePath();
+	}
 	// 怒り目
-	if(355/*悪いを10回押した*/ < eye_control_y)
+	if(temp_temp <= -10)
 	{
 		//左目
 		ctx.beginPath();
@@ -69,6 +87,7 @@ function CreateEye(start_x, start_y, end_x, end_y, control_y, eye_width)
 		ctx.fill();
 	}
 }
+
 function CreateMouth(start_x, start_y, end_x, end_y, control_y)
 {
 	//control_yで口の開き具合
@@ -84,8 +103,7 @@ function CreateHead(n, dx, dy, radi_big)
 {
 	var angle_big = Math.PI/n;
 	var angle_small = 1/6 + Math.PI/n;
-	var radi_small = 150;//固定
-	var radi_inside = 130;
+
 	ctx.beginPath();
 	for(var i = 0; i < 2 * Math.PI; i += angle_big)
 	{
@@ -108,19 +126,25 @@ function CreateHead(n, dx, dy, radi_big)
 
 function good_eye()
 {
-	if(eye_control_y <= chara_center_y - 50/*初期値*/)
+	temp_temp += 1;
+	// ただの線
+	if(temp_temp >= 0/*初期値*/)
 	{
+				console.log("良いボタンでただの線");
 		eye_control_y -= 3;
 		eye_pos_y += 3
 	}
+
 	// 目玉あり
-	if(chara_center_y - 50/*初期値*/ < eye_control_y && eye_control_y <= 355/*悪いを10回押した*/)
+	if(-10 < temp_temp && temp_temp < 0)
 	{
+				console.log("良いボタンでw目玉");
 		eye_control_y -= 3;
 	}
 	// 怒り目
-	if(355/*悪いを10回押した*/ < eye_control_y)
+	if(temp_temp <= -10)
 	{
+				console.log("良いボタンで怒り目");
 		eye_angle -= 1/16 * Math.PI;
 		eye_control_y -= 3;
 	}
@@ -128,34 +152,42 @@ function good_eye()
 }
 function bad_eye()
 {
+	temp_temp -=1;
 	//ただの線
-	if(eye_control_y <= chara_center_y - 50/*初期値*/)
+	if( 0 <=temp_temp)
 	{
+				console.log("悪いボタンでただの線");
 		eye_control_y += 3;
 		eye_pos_y -= 3;
 	}
 	// 目玉あり
-	if(chara_center_y - 50/*初期値*/ < eye_control_y && eye_control_y <= 355/*悪いを10回押した*/)
+	if(-10 < temp_temp && temp_temp < 0)
 	{
+				console.log("悪いボタンで目玉");
 		eye_control_y += 3;
 	}
 	// 怒り目
-	if(355/*悪いを10回押した*/ < eye_control_y)
+	if(-10 >= temp_temp)
 	{
+		console.log("悪いボタンで怒り目");
 		eye_angle += 1/16 * Math.PI;
 		eye_control_y += 3;
 	}
 	CleateCharacter();
 }
+
+
+
+
 function good_head()
 {
-	head_num -= 1;
+	// head_num -= 1;
 	head_big_radi -= 7;
 	CleateCharacter();
 }
 function bad_head()
 {
-	head_num += 1;
+	// head_num += 1;
 	head_big_radi += 7;
 	CleateCharacter();
 }
@@ -173,13 +205,12 @@ function bad_mouth()
 }
 function CleateCharacter()
 {
+	console.log("現在の" + temp_temp);
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	CreateHead(head_num, chara_center_x, chara_center_y, head_big_radi);
-	CreateMouth(chara_center_x - 100, mouth_pos_y, chara_center_x + 100, mouth_pos_y, mouth_control_y);
+	CreateMouth(chara_center_x - 50, mouth_pos_y, chara_center_x + 50, mouth_pos_y, mouth_control_y);
 	CreateEye(chara_center_x - (eye_end_to_center + eye_width), eye_pos_y, chara_center_x - eye_end_to_center , eye_pos_y, eye_control_y, eye_to_eye + eye_end_to_center);
 }
-
-
 function good_all()
 {
 	good_head();
