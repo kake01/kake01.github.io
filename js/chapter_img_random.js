58dc06a8c0dbd97ef3e9ddb72b2a8e8407ed6e34
kake01 <kakerun1123@gmail.com>
2019-11-22 19:03:17 +0900

    菌の複製完了

--- js/chapter_img_random.js
+++ js/chapter_img_random.js
@@ -1,68 +1,70 @@
 //内部の変数 => パラメーターの変更はしない
 var canvas = document.getElementById('rectangle');
 var ctx = canvas.getContext('2d');
-var coordinate = 0;
 
 //外部
 var head_num = 8;
-var color = ["#000000", "#f0f8ff", "#66FF99", "#FFFFFF"];
-var head_count = [0,0,0,0];
-var mouth_count = [0,0,0,0];
-var eye_count = [0,0,0,0];
+var eye_count = [];
+var head_count = [];
+var mouth_count = [];
+var virus_num = 30;
+// var color = ["#000000", "#f0f8ff", "#66FF99", "#FFFFFF"];
+var color = [];
+// var scale = [1.3,0.4,2,1.4];
+var chara_x = [];
+var chara_y = [];
+var scale = [];
+for(var i = 0; i < virus_num; i++)
+{
+  chara_x.push(Math.random() * canvas.width);
+  chara_y.push(Math.random() * canvas.height);
+  eye_count .push(0/*プレイヤーによって違う*/);
+  mouth_count.push(0/*プレイヤーによって違う*/);
+  head_count.push(0/*プレイヤーによって違う*/);
 
-var scale = [1.3,1.3,2,1.4];
 
-var original_chara_x = [200,500,700,200];
-var original_chara_y = [475,500,400,200];
+  let r = ('0' + Math.floor(Math.random() * 255).toString(16)).slice(-2);
+  let g = ('0' + Math.floor(Math.random() * 255).toString(16)).slice(-2);
+  let b = ('0' + Math.floor(Math.random() * 255).toString(16)).slice(-2);
 
-//元の位置に戻すのに使う
-var chara_x = original_chara_x.slice(0, original_chara_x.length);
-var chara_y = original_chara_y.slice(0, original_chara_y.length);
+  color.push('#' + r + g + b);
+  scale.push(Math.random() + 1);
+}
 
 onload = function()
 {
   CreateHead(head_num, chara_x, chara_y, 0, scale, color);
   CreateMouth(chara_x , chara_y, 0, scale);
   CreateEye(chara_x, chara_y, 0, scale);
-  //
-  setInterval("TimeUpDate()", 500);
+  setInterval("TimeUpDate()", 200);
 };
-
 function TimeUpDate()
 {
-  coordinate++;
+  for(var i = 0; i < head_count.length; i++)
+  {
+    chara_x[i] += Math.random() * 4 -2;
+    chara_y[i] += Math.random() * 4 -2;
 
-  if(coordinate == 3)
-  {
-    for(var i = 0; i < head_count.length; i++)
+    //範囲外に出た場合の処理
+    if(chara_x[i] <= 0)
     {
-      chara_x[i] = original_chara_x[i];
-      chara_y[i] = original_chara_y[i];
+      chara_x[i] = 2;
     }
-    coordinate = 0;
-  }
-  if(coordinate == 2)
-  {
-    for(var i = 0; i < head_count.length; i++)
+    if(canvas.width <= chara_x[i])
     {
-      chara_x[i] =  original_chara_x[i] + Math.random(4) -2;
-      chara_y[i] =  original_chara_y[i] + Math.random(4) -2;
+      chara_x[i] = canvas.width - 2;
     }
-  }
-  if(coordinate == 1)
-  {
-    for(var i = 0; i < head_count.length; i++)
+    if(chara_y[i] <= 0)
     {
-      chara_x[i] = original_chara_x[i] + Math.random(4) -2;
-      chara_y[i] = original_chara_y[i] + Math.random(4) -2;
+      chara_y[i] = 2;
+    }
+    if(canvas.height <= chara_y[i])
+    {
+      chara_y[i] = canvas.height  - 2;
     }
   }
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  CreateHead(head_num, chara_x, chara_y, 0, scale, color);
-  CreateMouth(chara_x , chara_y, 0, scale);
-  CreateEye(chara_x, chara_y, 0, scale);
+  CleateCharacter(0,0,0);
 }
-
 function CreateHead(n, dx, dy, counter, size, color)
 {
   for(var i = 0; i < head_count.length; i++)
@@ -70,7 +72,7 @@ function CreateHead(n, dx, dy, counter, size, color)
     head_count[i] -= counter;
     var angle_big = Math.PI / n;
     var angle_small = 1/6 + Math.PI / n;
-    var radi_small = 80 ;//固定
+    var radi_small = 25 ;//固定
     var radi_inside = radi_small * size[i];//固定,引数で変化
 
     //良くなった場合
@@ -105,17 +107,47 @@ function CreateHead(n, dx, dy, counter, size, color)
     ctx.arc(dx[j], dy[j], radi_small, 0, 2 * Math.PI, false);
   }
 }
+function CreateMouth(pos_x, pos_y, counter, size)
+{
+  for(var i = 0; i < mouth_count.length; i++)
+  {
+    mouth_count[i] += counter;
+    var mouth_start_x = pos_x[i] - 20 * size[i];
+    var mouth_end_x = pos_x[i] + 20 * size[i];
+    var mouth_center_y = pos_y[i] + 10 * size[i];
+    //良くなった場合
+    if(mouth_count[i] <= -5)
+    {
+      var mouth_control_y = mouth_center_y - 5 * 3;
+    }
+    //悪くなった場合
+    if(mouth_count[i] >= 5)
+    {
+      var mouth_control_y = mouth_center_y + 5 * 3;
+    }
+    if(-5 < mouth_count[i] && mouth_count[i] < 5)
+    {
+      var mouth_control_y = mouth_center_y + mouth_count[i] * 3;
+    }
+    ctx.beginPath();
+    ctx.moveTo(mouth_start_x, mouth_center_y);
+    ctx.quadraticCurveTo(pos_x[i], mouth_control_y, mouth_end_x, mouth_center_y);
+    ctx.strokeStyle = "Orange";
+    ctx.lineWidth = 3;
+    ctx.stroke();
+  }
+}
 function CreateEye(chara_center_x, chara_center_y, counter, size)
 {
   for(var i = 0; i < eye_count.length; i++)
   {
     eye_count[i] += counter;
     //目の中央
-    var eye_left_center_x = chara_center_x[i] - 40 * size[i];
-    var eye_rigth_center_x = chara_center_x[i] + 40 * size[i];
+    var eye_left_center_x = chara_center_x[i] - 10 * size[i];
+    var eye_rigth_center_x = chara_center_x[i] + 10 * size[i];
     //目の長さ
-    var eye_width = 20 * size[i];
-    var eye_center_y = chara_center_y[i] - 25 * size[i];
+    var eye_width = 7 * size[i];
+    var eye_center_y = chara_center_y[i] - 10 * size[i];
     //左目の右目 start点,end点
     var eye_left_start_x = eye_left_center_x - eye_width;
     var eye_left_end_x = eye_left_center_x + eye_width;
@@ -174,12 +206,12 @@ function CreateEye(chara_center_x, chara_center_y, counter, size)
       var eye_angle = Math.abs(10 + eye_count[i]);
       //左目
       ctx.beginPath();
-      ctx.arc(eye_left_center_x, eye_center_y, 25, 1/32*Math.PI * eye_angle, Math.PI + 1/32*Math.PI * eye_angle, false);
+      ctx.arc(eye_left_center_x, eye_center_y, 8 * size[i], 1/32*Math.PI * eye_angle, Math.PI + 1/32*Math.PI * eye_angle, false);
       ctx.fillStyle = 'rgb(0, 255, 0)';
       ctx.fill();
       //右目
       ctx.beginPath();
-      ctx.arc(eye_rigth_center_x, eye_center_y, 25, -1/32 * Math.PI * eye_angle, Math.PI - 1/32 * Math.PI * eye_angle, false);
+      ctx.arc(eye_rigth_center_x, eye_center_y, 8 * size[i], -1/32 * Math.PI * eye_angle, Math.PI - 1/32 * Math.PI * eye_angle, false);
       ctx.fillStyle = 'rgb(0, 255, 0)';
       ctx.fill();
     }
@@ -189,76 +221,29 @@ function CreateEye(chara_center_x, chara_center_y, counter, size)
       var eye_angle = Math.abs(10 - 13);
       //左目
       ctx.beginPath();
-      ctx.arc(eye_left_center_x, eye_center_y, 25, 1/32*Math.PI * eye_angle, Math.PI + 1/32*Math.PI * eye_angle, false);
+      ctx.arc(eye_left_center_x, eye_center_y, 8 * size[i], 1/32*Math.PI * eye_angle, Math.PI + 1/32*Math.PI * eye_angle, false);
       ctx.fillStyle = 'rgb(0, 255, 0)';
       ctx.fill();
       //右目
       ctx.beginPath();
-      ctx.arc(eye_rigth_center_x, eye_center_y, 25, -1/32 * Math.PI * eye_angle, Math.PI - 1/32 * Math.PI * eye_angle, false);
+      ctx.arc(eye_rigth_center_x, eye_center_y, 8 * size[i], -1/32 * Math.PI * eye_angle, Math.PI - 1/32 * Math.PI * eye_angle, false);
       ctx.fillStyle = 'rgb(0, 255, 0)';
       ctx.fill();
     }
   }
 }
-function CreateMouth(pos_x, pos_y, counter, size)
-{
-  for(var i= 0; i < mouth_count.length; i++)
-  {
-    mouth_count[i] += counter;
-    var mouth_start_x = pos_x[i] - 45 * size[i];
-    var mouth_end_x = pos_x[i] + 45 * size[i];
-    var mouth_center_y = pos_y[i] + 25 * size[i];
-
-    //良くなった場合
-    if(mouth_count[i] <= -5)
-    {
-      var mouth_control_y = mouth_center_y - 5 * 10;
-      //control_yで口の開き具合
-      ctx.beginPath();
-      ctx.moveTo(mouth_start_x, mouth_center_y);
-      ctx.quadraticCurveTo(pos_x[i], mouth_control_y, mouth_end_x, mouth_center_y);
-      ctx.strokeStyle = "Orange";
-      ctx.lineWidth = 5;
-      ctx.stroke();
-    }
-    //悪くなった場合
-    if(mouth_count[i] >= 5)
-    {
-      var mouth_control_y = mouth_center_y + 5 * 10;
-      //control_yで口の開き具合
-      ctx.beginPath();
-      ctx.moveTo(mouth_start_x, mouth_center_y);
-      ctx.quadraticCurveTo(pos_x[i], mouth_control_y, mouth_end_x, mouth_center_y);
-      ctx.strokeStyle = "Orange";
-      ctx.lineWidth = 5;
-      ctx.stroke();
-    }
-    // else
-    if(-5 < mouth_count[i] && mouth_count[i] < 5)
-    {
-      var mouth_control_y = mouth_center_y + mouth_count[i] * 10;
-      //control_yで口の開き具合
-      ctx.beginPath();
-      ctx.moveTo(mouth_start_x, mouth_center_y);
-      ctx.quadraticCurveTo(pos_x[i], mouth_control_y, mouth_end_x, mouth_center_y);
-      ctx.strokeStyle = "Orange";
-      ctx.lineWidth = 5;
-      ctx.stroke();
-    }
-  }
-}
-function CleateCharacter(hed_counter, mouth_count, eye_count)
+function CleateCharacter(hed_count, mouth_count, eye_count)
 {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  CreateHead(head_num, chara_x, chara_y, hed_counter, scale, color);
+  CreateHead(head_num, chara_x, chara_y, hed_count, scale, color);
   CreateMouth(chara_x , chara_y, mouth_count, scale);
   CreateEye(chara_x, chara_y, eye_count, scale);
 }
-function good_all()
+function GoodAll()
 {
   CleateCharacter(1, 1, 1);
 }
-function bad_all()
+function BadAll()
 {
   CleateCharacter(-1, -1, -1);
 }
@@ -266,10 +251,10 @@ document.addEventListener('keydown', (event) => {
   var keyName = event.key;
   if(keyName == "a")
   {
-    good_all();
+    GoodAll();
   }
   if(keyName == "z")
   {
-    bad_all();
+    BadAll();
   }
 });
